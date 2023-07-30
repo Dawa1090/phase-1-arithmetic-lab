4399780ad494b384cea20caf7091a67c8dc5e5d1
Dawa Sherpa <dawasherpa1090@gmail.com>
2023-07-30 09:54:38 -0400

    arithmetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31, num2 = 2;
+let multiply = num1*num2;
 
-// Write your code here
+let a = Math.random();
+let random = parseInt(a + 1);
+
+let num3 = 16, num4 = 4;
+let mod = num3/num4;
+
+let set = [1,2,3,4,5,20, 12, 16];
+let max = Math.max(...set);
